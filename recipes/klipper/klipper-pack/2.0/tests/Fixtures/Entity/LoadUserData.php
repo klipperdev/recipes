ebe3f4bd27e9f59fe7e4a660b55079bc6f1a9abf
François Pluchino <francois.pluchino@klipper.dev>
2020-05-17 21:09:02 +0200

    Add example of user functional tests

---
+++ recipes/klipper/klipper-pack/2.0/tests/Fixtures/Entity/LoadUserData.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Tests\Fixtures\Entity;
+
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Klipper\Bundle\FunctionalTestBundle\Tests\DefaultAuthenticationInterface;
+use Klipper\Bundle\FunctionalTestBundle\Tests\DefaultAuthenticationTrait;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+
+/**
+ * Fixtures for user.
+ */
+class LoadUserData implements FixtureInterface, DefaultAuthenticationInterface
+{
+    use DefaultAuthenticationTrait;
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->flush($manager, [
+            $this->addUser('admin.test', 'admin@test.tld'),
+            $this->addUser($this->defaultUsername, 'user@test.tld'),
+            $this->addUser('user.test.alone', 'user.alone@test.tld'),
+        ]);
+    }
+
+    /**
+     * @param User[] $users
+     */
+    protected function flush(ObjectManager $manager, array $users): void
+    {
+        foreach ($users as $user) {
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    protected function addUser(string $username, string $email, array $roles = [])
+    {
+        $user = new User();
+        $user->setUsername($username);
+        $user->setEmail($email);
+        $user->setPassword($this->defaultPassword);
+        $user->setRoles($roles);
+
+        return $user;
+    }
+}
---
+++ recipes/klipper/klipper-pack/2.0/tests/Functional/Model/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the Checkinn Platform package.
+ *
+ * (c) Checkinn <developer@checkinn.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\Functional\Model;
+
+use App\Entity\User;
+use App\Tests\Fixtures\Entity\LoadUserData;
+use Klipper\Bundle\FunctionalTestBundle\Tests\WebTestCase;
+
+/**
+ * Model user tests.
+ *
+ * @internal
+ */
+final class UserTest extends WebTestCase
+{
+    public function testUserEmail(): void
+    {
+        $this->loadFixtures([
+            LoadUserData::class,
+        ]);
+
+        $user = $this->findOneBy(User::class, ['email' => 'user@test.tld']);
+
+        static::assertSame('user@test.tld', $user->getEmail());
+
+        $test = new User();
+        $test->setEmail('test@test.tld');
+        $test->setPassword('test');
+        $this->create($test);
+
+        $test2 = $this->findOneBy(User::class, ['id' => $test->getId()]);
+        static::assertSame($test->getEmail(), $test2->getEmail());
+    }
+}
