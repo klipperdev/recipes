bbf1f786b016f70e1714b89084e5191d47ace8ab
François Pluchino <francois.pluchino@klipper.dev>
2020-06-08 15:12:37 +0200

    Add custom styles for bow recipe

--- recipes/klipper/bow-bundle/2.0/assets/app/main.ts
+++ recipes/klipper/bow-bundle/2.0/assets/app/main.ts
@@ -3,6 +3,7 @@ import Vue from 'vue';
 import vuetify from '@app/plugins/vuetify';
 import App from '@app/components/App.vue';
 import '@klipper/bow/registerServiceWorker';
+import '@app/styles/app.scss';
 
 Vue.config.productionTip = false;
 
