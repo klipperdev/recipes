8dad27456e7f055160a5c71e3b1c437392588a5a
François Pluchino <francois.pluchino@klipper.dev>
2020-06-09 12:06:20 +0200

    Update recipe of bow bundle

--- recipes/klipper/bow-bundle/2.0/assets/app/main.ts
+++ recipes/klipper/bow-bundle/2.0/assets/app/main.ts
@@ -1,13 +1,60 @@
 import 'core-js/stable';
 import Vue from 'vue';
-import vuetify from '@app/plugins/vuetify';
+import VueI18n from 'vue-i18n';
+import Vuex from 'vuex';
+import Vuetify from 'vuetify/lib';
+import VueSnackbar from '@klipper/bow/snackbars/VueSnackbar';
+import VueFormatter from '@klipper/bow/formatter/VueFormatter';
+import VueI18nExtra from '@klipper/bow/i18n/VueI18nExtra';
 import App from '@app/components/App.vue';
+import {I18nModule} from '@klipper/bow/stores/i18n/I18nModule';
+import {CurrencyFormatter} from '@klipper/bow/i18n/CurrencyFormatter';
+import {RootState} from '@app/store/RootState';
+import bowLocaleEn from '@klipper/bow/translations/en';
+import bowLocaleFr from '@klipper/bow/translations/fr';
+import appLocaleEn from '@app/translations/en';
+import appLocaleFr from '@app/translations/fr';
+import vuetifyLocaleFr from 'vuetify/src/locale/fr';
+import vuetifyBowPreset from '@klipper/bow/vuetify/bowPreset';
 import '@klipper/bow/registerServiceWorker';
 import '@app/styles/app.scss';
 
 Vue.config.productionTip = false;
+Vue.use(VueI18n);
+Vue.use(Vuetify);
+Vue.use(Vuex);
+
+const vuetify = new Vuetify(Object.assign({}, vuetifyBowPreset, {
+    lang: {
+        locales: {
+            fr: vuetifyLocaleFr,
+        },
+    },
+}));
+
+const i18n = new VueI18n({
+    locale: 'en',
+    fallbackLocale: 'en',
+    messages: {
+        en: Object.assign({}, bowLocaleEn, appLocaleEn),
+        fr: Object.assign({}, bowLocaleFr, appLocaleFr),
+    },
+});
+
+const store = new Vuex.Store<RootState>({
+    state: {} as RootState,
+    modules: {
+        i18n: new I18nModule(i18n, vuetify),
+    },
+});
+
+Vue.use(new VueI18nExtra({currencyFormatter: new CurrencyFormatter(store)}));
+Vue.use(new VueSnackbar());
+Vue.use(new VueFormatter());
 
 new Vue({
+    i18n,
+    store,
     vuetify,
     render: (h) => h(App),
 }).$mount('#app');
---
+++ recipes/klipper/bow-bundle/2.0/assets/app/store/RootState.ts
@@ -0,0 +1,5 @@
+import {I18nModuleState} from '@klipper/bow/stores/i18n/I18nModuleState';
+
+export interface RootState extends
+    I18nModuleState {
+}
