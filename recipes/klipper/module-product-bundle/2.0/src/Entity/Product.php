3aea193cab4967a3e3c296a3aabee1cbe89ac592
François Pluchino <francois.pluchino@klipper.dev>
2020-11-15 23:52:35 +0100

    Add translation fro product model

--- recipes/klipper/module-product-bundle/2.0/src/Entity/Product.php
+++ recipes/klipper/module-product-bundle/2.0/src/Entity/Product.php
@@ -3,11 +3,18 @@
 namespace App\Entity;
 
 use App\Repository\ProductRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use JMS\Serializer\Annotation as Serializer;
 use Klipper\Component\DoctrineExtensionsExtra\Mapping\Annotation as KlipperMetadata;
 use Klipper\Component\Model\Traits\IdTrait;
+use Klipper\Component\Model\Traits\TranslatableInterface;
+use Klipper\Component\Model\Traits\TranslatableTrait;
 use Klipper\Module\ProductBundle\Model\AbstractProduct;
+use Klipper\Module\RepairBundle\Model\Traits\ProductBreakdownableInterface;
+use Klipper\Module\RepairBundle\Model\Traits\ProductBreakdownableTrait;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ProductRepository::class)
@@ -16,9 +23,17 @@ use Klipper\Module\ProductBundle\Model\AbstractProduct;
  *     indexes={
  *         @ORM\Index(name="idx_product_created_at", columns={"created_at"}),
  *         @ORM\Index(name="idx_product_updated_at", columns={"updated_at"}),
- *         @ORM\Index(name="idx_product_name", columns={"name"})
+ *         @ORM\Index(name="idx_product_name", columns={"name"}),
+ *         @ORM\Index(name="idx_product_reference", columns={"reference"}),
+ *         @ORM\Index(name="idx_product_code_ean13", columns={"code_ean13"}),
+ *         @ORM\Index(name="idx_product_code_upc", columns={"code_upc"}),
+ *         @ORM\Index(name="idx_product_can_be_sell", columns={"can_be_sell"}),
+ *         @ORM\Index(name="idx_product_can_be_sell", columns={"can_be_sell"}),
+ *         @ORM\Index(name="idx_product_price", columns={"price"})
  *     }
  * )
+ *
+ * @Gedmo\TranslationEntity(class="App\Entity\ProductTranslation")
  *
  * @KlipperMetadata\MetadataObject(
  *     defaultSortable="updated_at:desc"
@@ -26,7 +41,45 @@ use Klipper\Module\ProductBundle\Model\AbstractProduct;
  *
  * @Serializer\ExclusionPolicy("all")
  */
-class Product extends AbstractProduct
+class Product extends AbstractProduct implements ProductBreakdownableInterface, TranslatableInterface
 {
     use IdTrait;
+    use ProductBreakdownableTrait;
+    use TranslatableTrait;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     *
+     * @Gedmo\Translatable
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(max=255)
+     * @Assert\NotBlank
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $name = null;
+
+    /**
+     * @ORM\Column(type="string", length=128, nullable=true)
+     *
+     * @Gedmo\Translatable
+     *
+     * @Assert\Type(type="string")
+     * @Assert\Length(min=0, max=128)
+     *
+     * @Serializer\Expose
+     */
+    protected ?string $description = null;
+
+    /**
+     * @ORM\OneToMany(
+     *     targetEntity="App\Entity\ProductTranslation",
+     *     mappedBy="object",
+     *     fetch="EXTRA_LAZY",
+     *     orphanRemoval=true,
+     *     cascade={"persist"}
+     * )
+     */
+    protected ?Collection $translations = null;
 }
--- recipes/klipper/module-product-bundle/2.0/src/Repository/ProductRepository.php
+++ recipes/klipper/module-product-bundle/2.0/src/Repository/ProductRepository.php
@@ -5,6 +5,8 @@ namespace App\Repository;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Klipper\Component\DoctrineExtensionsExtra\Entity\Repository\Traits\TranslatableRepositoryInterface;
+use Klipper\Component\DoctrineExtensionsExtra\Entity\Repository\Traits\TranslatableRepositoryTrait;
 
 /**
  * @method null|Product find($id, $lockMode = null, $lockVersion = null)
@@ -12,8 +14,11 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Product[]    findAll()
  * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ProductRepository extends ServiceEntityRepository
+class ProductRepository extends ServiceEntityRepository implements
+    TranslatableRepositoryInterface
 {
+    use TranslatableRepositoryTrait;
+
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
