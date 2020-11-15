56e0948ba610a0f871f11e286b67f8081297aade
François Pluchino <francois.pluchino@klipper.dev>
2020-11-15 02:33:09 +0100

    Update default sortable of choice entity

--- recipes/klipper/klipper-pack/2.0/src/Entity/Choice.php
+++ recipes/klipper/klipper-pack/2.0/src/Entity/Choice.php
@@ -50,7 +50,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * @KlipperSecurityExtra\OrganizationalFilterOptionalAllFilterClass
  *
  * @KlipperMetadata\MetadataObject(
- *     defaultSortable="position:asc, label:asc"
+ *     defaultSortable="type:asc, position:asc, label:asc"
  * )
  *
  * @Serializer\ExclusionPolicy("all")
