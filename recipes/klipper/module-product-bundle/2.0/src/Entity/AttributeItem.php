253ce22d6c85749a20bc08d66acfcc44abfa3acc
François Pluchino <francois.pluchino@klipper.dev>
2021-08-02 15:14:06 +0200

    Update model of attribut item

--- recipes/klipper/module-product-bundle/2.0/src/Entity/AttributeItem.php
+++ recipes/klipper/module-product-bundle/2.0/src/Entity/AttributeItem.php
@@ -8,9 +8,12 @@ use Gedmo\Mapping\Annotation as Gedmo;
 use JMS\Serializer\Annotation as Serializer;
 use Klipper\Component\DoctrineExtensionsExtra\Mapping\Annotation as KlipperMetadata;
 use Klipper\Component\Model\Traits\IdTrait;
+use Klipper\Component\Model\Traits\SingleExternalableInterface;
+use Klipper\Component\Model\Traits\SingleExternalableTrait;
 use Klipper\Component\Model\Traits\TranslatableInterface;
 use Klipper\Component\Model\Traits\TranslatableTrait;
 use Klipper\Component\Security\Model\OrganizationInterface;
+use Klipper\Component\SecurityExtra\Doctrine\Validator\Constraints as KlipperSecurityDoctrineAssert;
 use Klipper\Module\ProductBundle\Model\AbstractAttributeItem;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -19,13 +22,23 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @ORM\Table(
  *     indexes={
+ *         @ORM\Index(name="idx_attribute_item_external_id", columns={"external_id"}),
  *         @ORM\Index(name="idx_attribute_item_created_at", columns={"created_at"}),
  *         @ORM\Index(name="idx_attribute_item_updated_at", columns={"updated_at"}),
+ *         @ORM\Index(name="idx_attribut_item_reference", columns={"reference"}),
  *         @ORM\Index(name="idx_attribute_item_label", columns={"label"})
+ *     },
+ *     uniqueConstraints={
+ *         @ORM\UniqueConstraint(name="uniq_attribut_item_reference", columns={"organization_id", "reference"})
  *     }
  * )
  *
  * @Gedmo\TranslationEntity(class="App\Entity\AttributeItemTranslation")
+ *
+ * @KlipperSecurityDoctrineAssert\OrganizationalUniqueEntity(
+ *     fields={"organization", "reference"},
+ *     ignoreNull=false
+ * )
  *
  * @KlipperMetadata\MetadataObject(
  *     deepSearchPaths={"attribute"},
@@ -33,10 +46,10 @@ use Symfony\Component\Validator\Constraints as Assert;
  *
  * @Serializer\ExclusionPolicy("all")
  */
-class AttributeItem extends AbstractAttributeItem implements
-    TranslatableInterface
+class AttributeItem extends AbstractAttributeItem implements SingleExternalableInterface, TranslatableInterface
 {
     use IdTrait;
+    use SingleExternalableTrait;
     use TranslatableTrait;
 
     /**
