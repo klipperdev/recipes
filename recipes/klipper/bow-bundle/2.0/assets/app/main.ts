41cb7db5a554ffe830f85cbb26e1f45bf17cce76
François Pluchino <francois.pluchino@klipper.dev>
2022-09-04 11:47:21 +0200

    Add default languages for vuetify preset

--- recipes/klipper/bow-bundle/2.0/assets/app/main.ts
+++ recipes/klipper/bow-bundle/2.0/assets/app/main.ts
@@ -4,6 +4,8 @@ import App from '@app/components/App.vue';
 import appLocaleEn from '@app/translations/en';
 import appLocaleEs from '@app/translations/es';
 import appLocaleFr from '@app/translations/fr';
+import vuetifyLocaleEs from 'vuetify/src/locale/es';
+import vuetifyLocaleFr from 'vuetify/src/locale/fr';
 import {RootState} from '@app/store/RootState';
 import {createApp} from '@klipper/bow/bow';
 import '@app/styles/fonts.scss';
@@ -15,6 +17,14 @@ const app = createApp<RootState>({
             en: appLocaleEn,
             es: appLocaleEs,
             fr: appLocaleFr,
+        },
+    },
+    vuetifyPreset: {
+        lang: {
+            locales: {
+                es: vuetifyLocaleEs,
+                fr: vuetifyLocaleFr,
+            },
         },
     },
     rootRedirectRoute: {name: 'home', params: {org: '_'}},
